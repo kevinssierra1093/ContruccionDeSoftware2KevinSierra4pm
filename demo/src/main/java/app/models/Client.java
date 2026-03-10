24361e052c64a1b188b7b0fdc1d848c496576acc
Kevin Santiago Sierra <kevin.sierra68@correo.tdea.edu.co>
2026-03-10 04:56:10 -0500

    algunas clases creadas

---
+++ demo/src/main/java/app/models/Client.java
@@ -0,0 +1,18 @@
+package app.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+
+public class Client extends Person {
+//La clase cliente hereda de la clase persona y agrega el código del cliente, el tipo de cliente y un indicador de si el cliente está activo o no.
+
+    private Long idClient;
+    private String clientType;
+    private boolean isActive;
+    
+}
---
+++ demo/src/main/java/app/models/Person.java
@@ -0,0 +1,23 @@
+package app.models;
+
+import java.sql.Date;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+// se importa el Getter, Setter y NoArgsConstructor de lombok para generar automáticamente los métodos getter, setter y el constructor sin argumentos.
+@Setter
+@Getter
+@NoArgsConstructor
+// se define la clase abstracta Person, que servirá como base para otras clases que representen personas en el sistema.
+public abstract class Person {
+
+    private long id;
+    private String name;
+    private String document;
+    private String phone;
+    private String email;
+    private String address;
+    private Date birthDate;
+
+}
